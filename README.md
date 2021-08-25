e57e75c61cf0210a580748de6c98230d0271ea45
Percelic <kkw9427@gmail.com>
2021-08-25 17:53:18 +0900

    210825 commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Go_Public_REMS_CatM1_TestAPI
 
 
-* REMS CatM1 프로토콜 통신 테스트용 HTTP API 서버
+* REMS CatM1 프로토콜 통신 테스트를 위한  HTTP API 서버
+
+ 1) 사용 언어 : go 
+
+ 2) 사용 프레임워크 : gin
+
+ 
--- src/main.go
+++ src/main.go
@@ -1,39 +1,41 @@
 package main
 
 import (
+	"log"
+	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// 키 발급 만료 제한시간 (unixtimestamp)
-const G_EXPIRE_LIMIT = 1
+// 키 발급 만료까지 제한시간 (unixtime , second )
+const G_EXPIRE_LIMIT = 3 * 60 * 60
 
 // 액세스 토큰 구조체
 type accessToken struct {
-	tk1     string
-	tk2     string
-	expired int64
+	tk1     string // 64 byte Access_token에서 앞부분 32 byte
+	tk2     string // 64 byte Access_token에서 뒷부분 32 byte
+	expired int64  // 만료 시각 (unixtime)
 }
 
 // 인증키 구조체
 type authKeyResponse struct {
-	rtu_id       string `json:"rtu_id"`
-	access_token string `json:"access_token"`
-	now          int64  `json:"now"`
-	expired      int64  `json:"expired"`
+	Rtu_id       string `json:"rtu_id"`
+	Access_token string `json:"access_token"`
+	Now          int64  `json:"now"`
+	Expired      int64  `json:"expired"`
 }
 
 // 요청 데이터 구조체
 type dataResponse struct {
-	cid   string `json:"cid"`
-	multi int    `json:"multi"`
-	data  string `json:"data"`
+	Cid   string `json:"cid"`
+	Multi int    `json:"multi"`
+	Data  string `json:"data"`
 }
 
 // 생성된 액세스 토큰 저장 배열
-// var keyArr []accessToken
+var stored_tokens []accessToken
 
 func main() {
 	StartServer()
@@ -46,77 +48,100 @@ func StartServer() {
 
 	api.GET("/auth", responseAuthKey)
 
-	api.GET("/lte/data", responseData)
+	api.POST("/lte/data", responseData)
 
 	router.Run(":8080")
+	// HTTPS 실행 시.. ( .crt , .key 인증서 파일이 main.go 경로에 같이 있어야 됨 )
+	// router.RunTLS(":8080","server.crt","server.key")
 }
 
 func responseAuthKey(c *gin.Context) {
 
 	now_unixtime := time.Now().Unix()
-	accessToken := accessToken{"01234567890ABCDEF01234567890ABCD", "EF0123456789ABCDEF0123456789ABCD", now_unixtime + G_EXPIRE_LIMIT}
 
-	authKey := authKeyResponse{}
-	authKey.rtu_id = ""
-	authKey.access_token = accessToken.tk1 + accessToken.tk2
-	authKey.now = now_unixtime
-	authKey.expired = accessToken.expired
+	token := generateToken()
+	// accessToken := accessToken{tk1: "1234", tk2: "5678", expired: 10230402045}
+	accessToken := accessToken{tk1: token.tk1, tk2: token.tk2, expired: token.expired}
+
+	var authKey authKeyResponse
+	authKey.Rtu_id = "00000001"
+	authKey.Access_token = accessToken.tk1 + accessToken.tk2
+	authKey.Now = now_unixtime
+	authKey.Expired = accessToken.expired
+
+	log.Print(c.Request.Header)
+
+	log.Print("Cid : ", c.Request.Header.Get("Cid"))
+	log.Print("Authorization : ", c.Request.Header.Get("Authorization"))
 
 	c.JSON(http.StatusOK, authKey)
 }
 
 func responseData(c *gin.Context) {
 
-	//data := dataResponse{}
+	// 입력 header 확인 프로세스
 
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "Ok"})
+	log.Print(c.Request.Header)
+	log.Print(c.Request.Body)
+
+	log.Print("tk1 : ", c.Request.Header.Get("tk1"))
+	log.Print("tk2 : ", c.Request.Header.Get("tk2"))
+
+	//c.IndentedJSON(http.StatusOK, data)
 }
 
-// func main() {
+// 액세스 토큰 객체 생성
+func generateToken() accessToken {
 
-// http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	tk1 := make([]byte, 32)
+	tk2 := make([]byte, 32)
 
-// 	log.Print(req.Header)
+	var token accessToken
 
-// 	w.Header().Set("Content-Type", "application/json")
+	for {
 
-// 	w.Write([]byte("hihi"))
+		// 0 ~ 9  : 0x30 ~ 0x39
+		// A ~ F  : 0x41 ~ 0x46
+		for i := 0; i < len(tk1); i++ {
+			// 0 ~ 15 사이 무작위 난수 입력
+			tk1[i] = byte(rand.Intn(16))
+			tk2[i] = byte(rand.Intn(16))
 
-// })
+			// HEX to ASCII 변환
+			if tk1[i] >= 10 {
+				tk1[i] = tk1[i] + 0x37
+			} else {
+				tk1[i] = tk1[i] + 0x30
+			}
 
-// http.HandleFunc("/v1/api/auth", func(w http.ResponseWriter, req *http.Request) {
+			if tk2[i] >= 10 {
+				tk2[i] = tk2[i] + 0x37
+			} else {
+				tk2[i] = tk2[i] + 0x30
+			}
+		}
 
-// 	req.Header.Get("CID")
-// 	req.Header.Get("Authorization")
+		token = accessToken{string(tk1), string(tk2), time.Now().Unix() + G_EXPIRE_LIMIT}
 
-// 	log.Print(req.Body)
+		if checkRepeat(token) {
+			break
+		}
+	}
 
-// 	w.Header().Set("Content-Type", "application/json")
+	stored_tokens = append(stored_tokens, token)
 
-// 	w.Write([]byte("hi"))
+	return token
+}
 
-// 	//resp := authResponse{}
-// 	//resp.rtu_id =
+// 생성 토큰 중복 확인
+func checkRepeat(token accessToken) bool {
+	0
 
-// })
+	for _, stored := range stored_tokens {
+		if stored.tk1 == token.tk1 {
+			return false
+		}
+	}
 
-// http.HandleFunc("/v1/api/lte/data", func(w http.ResponseWriter, req *http.Request) {
-
-// 	// REQ HEADER
-// 	// tk1 32byte
-// 	// tk2 32byte
-
-// 	// RESP PARAM
-// 	// cid
-// 	// multi
-// 	// data
-
-// })
-
-// http.ListenAndServe(":60000", nil)
-
-// // err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
-// // if err != nil {
-// // 	log.Fatal("ListenAndServe : ", err)
-// // }
-// }
+	return true
+}
